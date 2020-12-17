ae40fa0a4ca5b90cf70bba234c284e028fb9fecd
Daniel Cornelio <jdcornelio89@gmail.com>
2020-12-17 00:24:35 -0600

    Funciones

---
+++ FundamentosJS/02_Funciones/clase4-funciones.js
@@ -0,0 +1,13 @@
+var nombre ='Daniel',edad=31
+
+function imprimirEdad(n, e){
+    console.log(`${n} tiene ${e} años`)
+}
+imprimirEdad(nombre , edad)
+imprimirEdad('jose' , 31)
+imprimirEdad('liz' , 27)
+imprimirEdad('claudia' , 28)
+imprimirEdad(30,'jose')
+imprimirEdad()
+imprimirEdad('jose')
+
