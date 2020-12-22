5d5556ebcc185067afbee7b5abc2d62c9f759eac
Daniel Cornelio <jdcornelio89@gmail.com>
2020-12-22 13:32:06 -0600

    Estructura de Carpetas

---
+++ FundamentosJS/01-PrimerosPasos/03_Objetos/clase8-parametrosPorReferencia.js
@@ -0,0 +1,38 @@
+var daniel = {
+    nombre : 'daniel',
+    apellido : 'Cornelio',
+    edad : 31
+}
+
+var isaac = {
+    nombre : 'isaac',
+    apellido : 'carrillo',
+    edad : 31
+}
+
+function imprimirNombreEnMayusculas(persona) {
+    // var nombre = persona.nobre
+    var {nombre} = persona
+    console.log(nombre.toUpperCase())
+}
+
+// Reto 
+
+function imprimirNombreEnMayusculas(persona) {
+    // var nombre = persona.nombre
+    var {nombre, edad} = persona
+    var msj = `Hola mi nombre es ${nombre} y tengo ${edad} años`
+    console.log(msj)
+}
+
+function cumpleanios(persona){
+    return {
+        ...persona,
+        edad :persona.edad + 1
+    }
+}
+
+imprimirNombreEnMayusculas(daniel)
+imprimirNombreEnMayusculas(isaac)
+
+
