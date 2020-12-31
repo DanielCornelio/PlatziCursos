306524bb8c182f69ff28efe240bcee71152a80b8
Daniel Cornelio <jdcornelio89@gmail.com>
2020-12-31 11:34:19 -0600

    prototipos

---
+++ FundamentosJS/04-ProgramacionOrientadaObjetos/clase21-prototipos.js
@@ -0,0 +1,23 @@
+function Persona(nombre, apellido, altura) {
+    this.nombre = nombre
+    this.apellido = apellido
+    this.altura = altura
+}
+
+Persona.prototype.saludar = function() {
+    console.log(`Hola me llamo ${this.nombre} ${this.apellido}`)
+}
+Persona.prototype.esAlto = function() {
+    console.log(`Hola me llamo ${this.nombre} ${this.apellido} soy ${this.altura >= 1.8?'Alto':'Bajo'}`)
+}
+var daniel = new Persona('daniel', 'cornelio', 1.82)
+var jessica = new Persona('jessica', 'zuarts', 1.80)
+var marcela = new Persona('marcela', 'peralta', 1.77)
+
+daniel.saludar()
+jessica.saludar()
+marcela.saludar()
+
+daniel.esAlto()
+jessica.esAlto()
+marcela.esAlto()
