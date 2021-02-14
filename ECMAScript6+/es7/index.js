ef200a83c8a8bab5c17a7275d6d6327e93d14876
Daniel <jdcornelio89@gmail.com>
2021-02-13 20:52:54 -0600

    es7

---
+++ ECMAScript6+/es7/index.js
@@ -0,0 +1,12 @@
+// includes
+let numbers = [1,2,3,4,7,8]
+if(numbers.includes(7)){
+    console.log('si se encuentra el valor 7')
+}else{
+    console.log('No se encuentra')
+}
+// Potencia
+let base = 4
+let exponent = 3
+let result = base ** exponent
+console.log(result)
