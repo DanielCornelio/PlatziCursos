bc96c93bdccd99dd1bd15589da23790271735152
Daniel <jdcornelio89@gmail.com>
2021-02-13 21:20:59 -0600

    es8

--- ECMAScript6+/es8/index.js
+++ ECMAScript6+/es8/index.js
@@ -32,3 +32,27 @@ const data= {
     front:'Alej', // Puede existir
     back: 'Rel'
 }
+
+// Async Await
+const helloWorld = ()=>{
+    return new Promise((resolve, reject)=>{
+        (true)
+            ?setTimeout(()=>resolve('hello world'), 3000)
+            :reject(new Error('Test Error'))
+    })
+}
+const helloAsync = async () =>{
+    const hello = await helloWorld()
+    console.log(hello)
+}
+helloAsync();
+
+const anotherFunction = async()=>{
+    try{
+        const hello = await helloWorld()
+        console.log(hello)
+    }catch(error){
+        console.log(error)
+    }
+}
+anotherFunction()
